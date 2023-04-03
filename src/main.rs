3bec50b223c8869a5747433529be5caf03fd53df
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2023-04-03 21:06:18 +0200

    1.2 parsing command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+/// Search for a string in a file and display all matching lines.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to search for.
+    pattern: String,
+    /// The path to the file in which to search.
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{:?}, {:?}", args.pattern, args.path);
 }
